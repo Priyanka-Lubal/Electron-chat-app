adf7e933599d3891d7ce8f9a86c9c44ac036f0da
Priyanka Lubal <Priyanka-Lubal@users.noreply.github.com>
2017-10-02 18:20:50 +0530

    Signup functionality added

--- ui.js
+++ ui.js
@@ -1,9 +1,15 @@
 /* eslint-env browser, node */
 
 const marked = require('marked');
+var mongo = require('mongodb').MongoClient;
 
 let socket;
 let username;
+let password;
+let screen="common chat"
+var collection,collection1;
+var flag=0;
+var usersid = {};
 
 window.onload = function () {
   initEvents();
@@ -15,30 +21,67 @@ function $(sel) {
 
 function initSocket() {
   const server = $('#server-url').value.trim();
+  $('#chat-active').innerHTML = 'Common Channel';
   appendText(`Connecting...`);
   socket = io.connect(server);
   socket.on('connect', () => {
     appendText(`Connected to server ${server}`);
+    mongo.connect('mongodb://127.0.0.1/message', function (err, db) {
+          collection = db.collection('messages')
+          collection1 = db.collection('profile')
+          if(flag==0){
+          collection1.insert({ username:username,password: password }, function (err, o) {
+                if (err) { console.warn(err.message); }
+                else { console.log("user inserted into db"); }
+            });
+          }
+          else
+          {
+            collection1.find({"username":username,"password":password},function(err,res){
+              if(res.length)
+                console.log("Success!!");
+              else
+                console.log("Invalid user or password!!");
+            });
+          }
+          $('#hello').innerHTML = "Hello "+ username;
+          console.log("HereIam:");
+          collection.find( {receiver : "common chat"} ).sort({ _id : -1 }).limit(10).toArray(function(err,res){
+                      if(err) throw err;
+                      //io.sockets.emit('bulk',res);
+                      if(res.length){
+                        for(var x=res.length-1;x>=0;x=x-1){
+                          appendText(`__${res[x].sender}:__ ${res[x].message}`);
+                        }
+                      }
+                      console.log("HereIam:",res);
+                  });
+                });
   });
-  socket.on('bulk', (data) => {
-    if(data.length){
-      for(var x=data.length-1;x>=0;x=x-1){
-        appendText(`__${data[x].name}:__ ${data[x].message}`);
-      }
-    }
-  });
+  // socket.on('bulk', (data) => {
+  //   if(data.length){
+  //     for(var x=data.length-1;x>=0;x=x-1){
+  //       appendText(`__${data[x].name}:__ ${data[x].message}`);
+  //     }
+  //   }
+  // });
   socket.on('message', (data) => {
-    appendText(`__${data.username}:__ ${data.text}`);
+    console.log(data);
+   if(screen == data.username )
+      appendText(`__${data.username}:__ ${data.text}`);
   });
   socket.on('login', (data) => {
-  //  appendText(`${data.username} has logged in.`);
+    appendText(`${data.username} has logged in.`);
+    usersid = data.usersid;
+    console.log(usersid);
     updateUserList(data.users);
   });
+
   socket.on('typing', (data) => {
-    setStatus(`${data.username} is typing...`);
+    //setStatus(`${data.username} is typing...`);
   });
   socket.on('stop-typing', () => {
-    setStatus('');
+    //setStatus('');
   });
   socket.on('logout', (data) => {
     appendText(`${data.username} disconnected.`);
@@ -53,9 +96,55 @@ function initEvents() {
   let typingTimer;
   let typing = false;
 
+  console.log("username",username);
+
   $('#text-input').addEventListener('keydown', function (e) {
     if (e.keyCode === 13) {
       sendText();
+    }
+  });
+  $('#users').addEventListener('click',function(e){
+    if(e.target.innerHTML!= "common chat"){
+      console.log(usersid[e.target.innerHTML]);
+    }
+    if(username != e.target.innerHTML && e.target.innerHTML != screen){
+      screen = e.target.innerHTML;
+      $('#chat-active').innerHTML = e.target.innerHTML;
+      if(screen == 'common chat'){
+        mongo.connect('mongodb://127.0.0.1/message', function (err, db) {
+              collection = db.collection('messages');
+              console.log("HereIam:");
+              collection.find( {receiver : "common chat"} ).sort({ _id : -1 }).limit(10).toArray(function(err,res){
+                    if(err) throw err;
+                    //io.sockets.emit('bulk',res);
+                    if(res.length){
+                      for(var x=res.length-1;x>=0;x=x-1){
+                        appendText(`__${res[x].sender}:__ ${res[x].message}`);
+                      }
+                    }
+                    console.log("collection",res);
+                });
+              });
+      }
+      else{
+        mongo.connect('mongodb://127.0.0.1/message', function (err, db) {
+              collection = db.collection('messages')
+              console.log("Current screen:",screen);
+              collection.find( { $or: [{receiver:screen},{receiver:username},{sender:screen},{sender:username}]} ).sort({ _id : -1 }).limit(10).toArray(function(err,res){
+                          if(err) throw err;
+                          //io.sockets.emit('bulk',res);
+                          if(res.length){
+                            for(var x=res.length-1;x>=0;x=x-1){
+                              if(res[x].receiver!='common chat')
+                                appendText(`__${res[x].sender}:__ ${res[x].message}`);
+                            }
+                          }
+                          console.log("HereIam:",res);
+                      });
+                    });
+      }
+      $('#chat-text').innerHTML = '';
+      console.log(e.target.innerHTML);
     }
   });
   $('#text-input').addEventListener('input', function () {
@@ -72,25 +161,55 @@ function initEvents() {
       socket.emit('stop-typing', { username });
     }, 1000);
   });
-  $('#username').addEventListener('keydown', function (e) {
+
+  $('#lpassword').addEventListener('keydown', function (e) {
     if (e.keyCode === 13) {
-      const value = this.value.trim();
-      if (value) {
-        username = this.value;
+      const value1 = this.value.trim();
+      const value = $('#lusername').value.trim();
+      if (value && value1) {
+        username = value;
+        password = value1;
+        flag=1;
         initSocket();
         login();
       }
     }
   });
+
+  $('#password').addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+      const value1 = this.value.trim();
+      const value = $('#username').value.trim();
+      if (value && value1) {
+        username = value;
+        password = value1;
+        initSocket();
+        login();
+      }
+    }
+  });
+
+  // $('#login').addEventListener('click', function (e) {
+  //     const value = this.value.trim();
+  //     if (value) {
+  //       username = this.value;
+  //       initSocket();
+  //       login();
+  //     }
+  // });
+
   $('#send-btn').addEventListener('click', sendText);
   $('#username').focus();
+  $('#password').focus();
 }
 
 function sendText() {
   const inputField = $('#text-input');
   const text = inputField.value.trim();
   if (!text) return;
-  socket.emit('message', { username, text });
+  socket.emit('message', { username, text , screen });
+  if(screen!= 'common chat')
+    appendText(`__${username}:__ ${text}`);
   inputField.value = '';
 }
 
@@ -117,6 +236,8 @@ function updateUserList(users) {
 
 function login() {
   socket.emit('login', { username });
+  
+  $('#signup-box').classList.add('hidden');
   $('#login-box').classList.add('hidden');
   $('#text-input').focus();
 }
